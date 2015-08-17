4a09ba3583ca2fb1f6843d46958cbe5c34ecf141
snorret <johan.ung@gmail.com>
2015-08-17 22:51:37 +0200

    Added example test controller and index.html page

---
+++ src/Skitlager/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+
+namespace Test.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Skeeetlager!";
+        }
+    }
+
+}
