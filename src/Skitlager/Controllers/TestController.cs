fe3974cd2e04c1394740d2d5d2c205b175b43d8f
snorr <snorr@localhost.localdomain>
2015-09-01 10:21:41 +0200

    Added example to get the app base path (need the full path for Sqlite db file setup)

--- src/Skitlager/Controllers/TestController.cs
+++ src/Skitlager/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Runtime;
 using System.Collections.Generic;
 
 namespace Test.Controllers
@@ -7,10 +8,18 @@ namespace Test.Controllers
     [Route("api/[controller]")]
     public class TestController : Controller
     {
+        private readonly IApplicationEnvironment _appEnvironment;
+
+        public TestController(IApplicationEnvironment environment)
+        {
+            _appEnvironment = environment;
+        }
+
         [HttpGet]
         public string Get()
         {
-            return "Skeeetlager!";
+            string toReturn = "Skeeeetlager: " + _appEnvironment.ApplicationBasePath;
+            return toReturn;
         }
     }
 
